76abd6ae37cf856005221e8a3f1a0d2f9ee38286
Justin Reeves <reeves122@gmail.com>
2024-03-21 16:38:26 -0500

    create DiffParser unit tests

--- diff_parser/test_diff_parser.py
+++ diff_parser/test_diff_parser.py
@@ -1,49 +1,59 @@
 """Provide tests for diff_parser."""
-import unittest
-
-from typing_extensions import Self
+import pytest
 
 from diff_parser.diff_parser import DiffParser
 
 
-class TestDiffParser(unittest.TestCase):
-    """Provide tests for diff_parser."""
+def test_get_repo_commit_changes() -> None:
+    """Validate the get_repo_commit_changes method is successful."""
+    changes = DiffParser.get_repo_commit_changes(
+        unified_diff=[
+            '--- \n',
+            '+++ \n',
+            '@@ -1,2 +1,2 @@\n',
+            '-test_repo_1 = "123.foo.com/test-repo-1:abc123"',
+            '+test_repo_1 = "123.foo.com/test-repo-1:abc456"',
+            '-test_repo_2 = "123.foo.com/bar/test-repo-2:def123"',
+            '+test_repo_2 = "123.foo.com/bar/test-repo-2:def456"'
+            '-foo         = "bar1"',
+            '+foo         = "bar2"'
+        ]
+    )
+    assert changes == {
+        'test-repo-1': 'abc456',
+        'test-repo-2': 'def456'
+    }
 
-    def test_parse_repo_name(self: Self) -> None:
-        """
-        A repo name should be parsed from a line of text.
 
-        :return:
-        """
-        self.assertEqual('test-repo-1',
-                         DiffParser._parse_repo_name('test_repo_1 = "123.foo.com/bar/test-repo-1:abc123"'))
-        self.assertEqual('test-repo-1', DiffParser._parse_repo_name('test_repo_1 = "123.foo.com/test-repo-1:abc123"'))
-        self.assertEqual('test-repo-1', DiffParser._parse_repo_name('test_repo_1 = "test-repo-1:abc123"'))
-        self.assertIsNone(DiffParser._parse_repo_name('test_repo_1 = "123.foo.com/test:v1.2"'))
-        self.assertIsNone(DiffParser._parse_repo_name('test_repo_1 = "https://foo.com"'))
-        self.assertIsNone(DiffParser._parse_repo_name('bucket_arn = "arn:aws:s3:::foo-800000001"'))
-        self.assertIsNone(
-            DiffParser._parse_repo_name('snapshot = "arn:aws:rds:us-east-2:12345:cluster-202301062012000004"'))
-        self.assertIsNone(DiffParser._parse_repo_name('name_suffix : "read_only"'))
-        self.assertIsNone(
-            DiffParser._parse_repo_name('LOCATIONS = "classpath:flyway/migrations,classpath:flyway/foo/bar"'))
-        self.assertIsNone(DiffParser._parse_repo_name('  JAVA_OPTS = "--add-opens -javaagent:/opt/foo/foo.jar"'))
+@pytest.mark.parametrize('test_input,expected', [
+    ('test_repo_1 = "123.foo.com/bar/test-repo-1:abc123"', 'test-repo-1'),
+    ('test_repo_1 = "123.foo.com/test-repo-1:abc123"', 'test-repo-1'),
+    ('test_repo_1 = "test-repo-1:abc123"', 'test-repo-1'),
+    ('test_repo_1 = "123.foo.com/test:v1.2"', None),
+    ('test_repo_1 = "https://foo.com"', None),
+    ('bucket_arn  = "arn:aws:s3:::foo-800000001"', None),
+    ('snapshot    = "arn:aws:rds:us-east-2:12345:cluster-202301062012000004"', None),
+    ('name_suffix : "read_only"', None),
+    ('LOCATIONS   = "classpath:flyway/migrations,classpath:flyway/foo/bar"', None),
+    ('  JAVA_OPTS = "--add-opens -javaagent:/opt/foo/foo.jar"', None),
+])
+def test_parse_repo_name(test_input: str, expected: str) -> None:
+    """Validate the _parse_repo_name method is successful."""
+    assert DiffParser._parse_repo_name(test_input) == expected
 
-    def test_parse_commit(self: Self) -> None:
-        """
-        A commit should be parsed from a line of text.
 
-        :return:
-        """
-        self.assertEqual('abc123', DiffParser._parse_commit('test_repo_1 = "123.foo.com/bar/test-repo-1:abc123"'))
-        self.assertEqual('abc123', DiffParser._parse_commit('test_repo_1 = "123.foo.com/test-repo-1:abc123"'))
-        self.assertEqual('abc123', DiffParser._parse_commit('test_repo_1 = "test-repo-1:abc123"'))
-        self.assertIsNone(DiffParser._parse_commit('test_repo_1 = "123.foo.com/test:v1.2"'))
-        self.assertIsNone(DiffParser._parse_commit('test_repo_1 = "https://foo.com"'))
-        self.assertIsNone(DiffParser._parse_commit('bucket_arn = "arn:aws:s3:::foo-800000001"'))
-        self.assertIsNone(
-            DiffParser._parse_commit('snapshot = "arn:aws:rds:us-east-2:12345:cluster-202301062012000004"'))
-        self.assertIsNone(DiffParser._parse_commit('name_suffix : "read_only"'))
-        self.assertIsNone(
-            DiffParser._parse_commit('LOCATIONS = "classpath:flyway/migrations,classpath:flyway/foo/bar"'))
-        self.assertIsNone(DiffParser._parse_commit('  JAVA_OPTS = "--add-opens -javaagent:/opt/foo/foo.jar"'))
+@pytest.mark.parametrize('test_input,expected', [
+    ('test_repo_1 = "123.foo.com/bar/test-repo-1:abc123"', 'abc123'),
+    ('test_repo_1 = "123.foo.com/test-repo-1:abc123"', 'abc123'),
+    ('test_repo_1 = "test-repo-1:abc123"', 'abc123'),
+    ('test_repo_1 = "123.foo.com/test:v1.2"', None),
+    ('test_repo_1 = "https://foo.com"', None),
+    ('bucket_arn  = "arn:aws:s3:::foo-800000001"', None),
+    ('snapshot    = "arn:aws:rds:us-east-2:12345:cluster-202301062012000004"', None),
+    ('name_suffix : "read_only"', None),
+    ('LOCATIONS   = "classpath:flyway/migrations,classpath:flyway/foo/bar"', None),
+    ('  JAVA_OPTS = "--add-opens -javaagent:/opt/foo/foo.jar"', None),
+])
+def test_parse_commit(test_input: str, expected: str) -> None:
+    """Validate the _parse_commit method is successful."""
+    assert DiffParser._parse_commit(test_input) == expected
