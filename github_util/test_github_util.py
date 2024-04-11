5418efade9f21efc9f6f20262022c659bc3a83a4
Justin Reeves <reeves122@gmail.com>
2024-04-11 17:09:21 -0500

    fix linting

--- github_util/test_github_util.py
+++ github_util/test_github_util.py
@@ -61,9 +61,6 @@ class TestGitHubUtil(unittest.TestCase):
 
     def test_tag_commit_with_ref_not_found(self: Self) -> None:
         """Validate the tag_commit function handles a ref not found."""
-        repo = MagicMock()
-        repo.get_commit.return_value = None
-        self.github_session.get_organization.return_value.get_repo.return_value = repo
-        self.github_session.get_organization.return_value.get_repo.return_value.get_git_ref.side_effect = UnknownObjectException(
-            400)
+        self.github_session.get_organization.return_value. \
+            get_repo.return_value.get_git_ref.side_effect = UnknownObjectException(400)
         self.assertIsNone(self.github_util.tag_commit(repo_name='test-repo-1', commit='123', tag='test-tag'))
