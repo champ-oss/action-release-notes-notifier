ba377fa1919253d5971657cafa4be1f3892220ac
Justin Reeves <reeves122@gmail.com>
2024-04-24 12:18:04 -0500

    Update Slack Notifier to accept message blocks

--- slack_notifier/slack_notifier.py
+++ slack_notifier/slack_notifier.py
@@ -17,37 +17,55 @@ class SlackNotifier:
         :param webhook_url: Webhook URL to send messages
         :param webhook_client: Optionally inject a WebhookClient
         """
+        self._message_blocks = []
         if not webhook_client:
             self._webhook_client = WebhookClient(webhook_url)
         else:
             self._webhook_client = webhook_client
 
-    def send_markdown(self: Self, message: str) -> None:
+    def add_message_block(self, message: str, at_beginning: bool = False) -> None:
         """
-        Send a message to Slack in Markdown format.
+        Add a message block to be sent to Slack.
 
-        :param message: message to send
+        :param message: text of the message
+        :param at_beginning: If true, add the message to the beginning (a header for example)
         :return: None
         """
-        if not message:
+        block = {
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': message
+            }
+        }
+        if at_beginning:
+            self._message_blocks.insert(0, block)
+        else:
+            self._message_blocks.append(block)
+
+    def has_messages(self: Self) -> bool:
+        """
+        Check if there are messages to send.
+
+        :return: True if there are messages, False otherwise
+        """
+        return len(self._message_blocks) > 0
+
+    def send_message(self: Self) -> None:
+        """
+        Send a message to Slack using the message blocks.
+
+        :return: None
+        """
+        if len(self._message_blocks) == 0:
             logger.info('not sending Slack message because message is empty')
             return
 
-        if len(message) > 3000:
-            logger.error('message is greater than the Slack limit of 3000 characters')
-            raise ValueError()
+        if len(self._message_blocks) > 50:
+            logger.warning('message is greater than the Slack limit of 50 blocks '
+                           '(https://api.slack.com/reference/block-kit/blocks#section)')
 
         logger.info('sending message to Slack')
-        response = self._webhook_client.send(
-            text='fallback',
-            blocks=[
-                {
-                    'type': 'section',
-                    'text': {
-                        'type': 'mrkdwn',
-                        'text': message
-                    }
-                }
-            ]
-        )
+        response = self._webhook_client.send(text='fallback', blocks=self._message_blocks)
         logger.info(f'response from Slack: {response.status_code} {response.body}')
+        assert response.status_code == 200
